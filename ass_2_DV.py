663d846f2e18241952ddb60e561dd1baf2b178c0
Darcy <darcyalexandra99@gmail.com>
2025-11-29 13:58:30 +0100

    Assignment 2 section 2

--- ass_2_DV.py
+++ ass_2_DV.py
@@ -3,16 +3,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy.stats as stats #used for sem & paired t-test
+import scipy.stats as stats #used for sem & paired t-test to test function
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
 neuralResponses_S1 = pd.read_csv('FilesForAssignment2_DV/NeuralResponses_S1.txt', sep=",")
 categoryLabels = pd.read_csv('FilesForAssignment2_DV/CategoryLabels.txt', sep=",")
 categoryVectors = pd.read_csv('FilesForAssignment2_DV/CategoryVectors.txt', sep=",")
+neuralResponses_S2 = pd.read_csv('FilesForAssignment2_DV/NeuralResponses_S2.txt', sep=",")
 
 #Averaging data per category
-animate = (categoryVectors['Var1'] == 1)
-inanimate = (categoryVectors['Var2'] == 1)
-animateObjects = neuralResponses_S1[animate]
-inanimateObjects = neuralResponses_S1[inanimate]
+animate_mask = (categoryVectors['Var1'] == 1)
+inanimate_mask = (categoryVectors['Var2'] == 1)
+animateObjects = neuralResponses_S1[animate_mask]
+inanimateObjects = neuralResponses_S1[inanimate_mask]
 
 averageAnimate = animateObjects.mean(axis=1) #axis=1 averages along de columns
 averageInanimate = inanimateObjects.mean(axis=1)
@@ -55,4 +59,121 @@ plt.title('Animate minus Inanimate')
 plt.xticks(np.arange(0,21,5)) #ensures the x-axis ticks go by increments of 5
 plt.bar(range(1,21), voxelDifference[:20],edgecolor='black')
 plt.axhline(0, color='black')
-plt.show()
+# plt.show()
+
+#Question 2A
+animateObjects_2 = neuralResponses_S2[animate_mask]
+inanimateObjects_2 = neuralResponses_S2[inanimate_mask]
+
+#training set (first 22 images)
+train_animate = animateObjects_2.iloc[:22]
+train_inanimate = inanimateObjects_2.iloc[:22]
+x_train = pd.concat([train_animate, train_inanimate]) #combines inanimate & animate into 1 training set
+y_train = [1] * 22 + [-1] * 22 #first 22 images are animate with label 1; last 22 images are inanimate with label -1
+
+#test set (last 22 images)
+test_animate = animateObjects_2.iloc[22:]
+test_inanimate = inanimateObjects_2.iloc[22:]
+x_test = pd.concat([test_animate, test_inanimate]) #combines inanimate & animate into 1 testing set
+y_test = [1] * 22 + [-1] * 22
+
+#train SVM model
+clf = svm.SVC(kernel='linear')
+clf.fit(x_train, y_train)
+
+#test the model
+prediction = clf.predict(x_test)
+
+print("the predicted outcomes of the SVM are:")
+print(prediction)
+
+print("the real answers:")
+print(y_test)
+
+#calculate the accuracy of the SVM
+accuracy = accuracy_score(y_test, prediction)
+print("the accuracy of the SVM is:")
+print(accuracy)
+
+#Question 2B
+svm_weights = clf.coef_[0, :20] #weights of the first 20 voxels
+
+averageVoxelAnimate_S2 = animateObjects_2.mean(axis=0) #axis=0 to average voxels (not responses)
+averageVoxelInanimate_S2 = inanimateObjects_2.mean(axis=0)
+voxelDifference_S2 = averageVoxelAnimate_S2-averageVoxelInanimate_S2
+voxelDifference_S2_20 = voxelDifference_S2[:20]
+
+#scatter plot weights vs. responses
+plt.figure(figsize=(7,7))
+plt.xlabel('Weights')
+plt.ylabel('Responses')
+plt.title('Scatter Plot')
+plt.scatter(svm_weights, voxelDifference_S2_20, color='none', edgecolor='blue')
+# plt.show()
+
+#Pearson's r
+r_matrix = np.corrcoef(svm_weights, voxelDifference_S2_20)
+r_value = r_matrix[0, 1]
+print("Pearson's r is:")
+print(r_value)
+
+#Question 2D
+human_mask = (categoryVectors['Var1'] == 1) & (categoryVectors['Var3'] == 1)
+inhuman_mask = (categoryVectors['Var1'] == 1) & (categoryVectors['Var4'] == 1)
+
+human_data = neuralResponses_S2[human_mask]
+inhuman_data = neuralResponses_S2[inhuman_mask]
+
+human_data = human_data.iloc[:-4] #delete the last 4 rows
+
+# print(human_data)
+# print(inhuman_data)
+
+# training set (first 10 images)
+train_human = human_data.iloc[:10]
+train_inhuman = inhuman_data.iloc[:10]
+x_human_train = pd.concat([train_human, train_inhuman])
+y_human_train = [1] * 10 + [-1] * 10
+
+#testing set (last 10 images)
+test_human = human_data.iloc[10:]
+test_inhuman = inhuman_data.iloc[10:]
+x_human_test = pd.concat([test_human, test_inhuman])
+y_human_test = [1] * 10 + [-1] * 10
+
+#train SVM model
+clf_human = svm.SVC(kernel='linear')
+clf_human.fit(x_human_train, y_human_train)
+
+#test model
+prediction_human = clf_human.predict(x_human_test)
+
+print("the predicted outcomes of the SVM are:")
+print(prediction_human)
+
+print("the real answers:")
+print(y_human_test)
+
+#calculate accuracy of SVM
+accuracy_human = accuracy_score(y_human_test, prediction_human)
+print("the accuracy of the SVM is:")
+print(accuracy_human)
+
+#calculate weights
+svm_weights_human = clf_human.coef_[0, :20] #weights of first 20 voxels
+#Average & difference of voxels for S2
+averageVoxel_human = human_data.mean(axis=0) #axis=0 to average voxels (not responses)
+averageVoxel_inhuman = inhuman_data.mean(axis=0)
+voxelDifference_human = averageVoxel_human - averageVoxel_inhuman
+voxelDifference_human = voxelDifference_human[:20] #take first 20 rows
+
+#scatter plot weights vs. responses
+plt.figure(figsize=(7,7))
+plt.xlabel('Weights')
+plt.ylabel('Responses')
+plt.title('Scatter Plot')
+plt.scatter(svm_weights_human, voxelDifference_human, color='none', edgecolor='blue')
+# plt.show()
+
+r_matrix_human = np.corrcoef(svm_weights_human, voxelDifference_human)
+print(r_matrix_human)
